3a8e0cfe932f66f054840a180861f73f29f337a5
Santosh <santoshkalwar@gmail.com>
2022-05-26 05:34:04 +0300

    Update bugwars.cpp

--- bugwars.cpp
+++ bugwars.cpp
@@ -1,7 +1,14 @@
-// Refueling: Given the locations of cities and the amount of fuel at each of them, compute the maximum number of cities that can be visited.
+// Refueling: Given the locations of cities and the amount of fuel at each of them, compute the maximum number of cities that can be visited
 
 #include "bits/stdc++.h"
-template<class T>bool umax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
+template<class T>bool umax(T& a,T b)
+{
+  if(a<b)
+  {
+    a=b;return 1;
+  }
+  return 0;
+}
 
 const int N=707;
 int dp[N][N][2];
@@ -14,12 +21,10 @@ int solution(vector<int> &a, vector<int> &x) {
     for(int len=0;len+1<n;len++){
         for(int i=0;i<n;i++){
             int j=i+len;
-            //k=0
             if(i and dp[i][j][0]>=x[i]-x[i-1])
                 umax(dp[i-1][j][0],dp[i][j][0]+a[i-1]-(x[i]-x[i-1]));
             if(j+1<n and dp[i][j][0]>=x[j+1]-x[i])
                 umax(dp[i][j+1][1],dp[i][j][0]+a[j+1]-(x[j+1]-x[i]));
-            //k=1
             if(i and dp[i][j][1]>=x[j]-x[i-1])
                 umax(dp[i-1][j][0],dp[i][j][1]+a[i-1]-(x[j]-x[i-1]));
             if(j+1<n and dp[i][j][1]>=x[j+1]-x[j])
